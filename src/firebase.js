2582fc27121a829fd6f0e440c7c5a9ceb376a65f
aditya-gupta-dev <142041364+aditya-gupta-dev@users.noreply.github.com>
2023-09-15 21:23:46 +0530

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyACdTukbllVK8srusflpscaffWng3jvGg0",
+    authDomain: "daring-phoenix-397715.firebaseapp.com",
+    projectId: "daring-phoenix-397715",
+    storageBucket: "daring-phoenix-397715.appspot.com",
+    messagingSenderId: "980240483567",
+    appId: "1:980240483567:web:0b0ecfcf0422072c6a071a",
+    measurementId: "G-8JLBF83JX4"
+};
+
+const app = initializeApp(firebaseConfig)
+
+const db = getFirestore(app)
+
+export const db;
